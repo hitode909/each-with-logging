584396cddf055472b081c47d8842a99638722ee0
hitode909 <hitode909@gmail.com>
2011-03-07 11:10:21 +0900

    caller

--- lib/each-with-logging.rb
+++ lib/each-with-logging.rb
@@ -2,7 +2,7 @@ module Enumerable
   def each_with_logging(logger = nil)
     len = self.length
     self.each_with_index{|value, index|
-      message = "#{caller.first} #{index+1} / #{len}"
+      message = "#{caller[3]} #{index+1} / #{len}"
       logger.respond_to?(:debug) ? logger.debug(message) : warn(message)
       yield value
     }
